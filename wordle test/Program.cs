2500d1d005f7ec5c4be495aaa4eeb6ed9d59caed
Gábor Miklós <miklos.gabor@blathy.info>
2026-03-30 22:00:07 +0200

    athome commit test + guess method basics

--- wordle test/Program.cs
+++ wordle test/Program.cs
@@ -5,26 +5,43 @@
 
         public class Wordle
         {
-            public char Letter1 { get; set; }
 
-            public char Letter2 { get; set; }
-
-            public char Letter3 { get; set; }
-
-            public char Letter4 { get; set; }
-
-            public char Letter5 { get; set; }
-
-            public Wordle(char letter1, char letter2, char letter3, char letter4, char letter5)
-            {
-                letter1 = Letter1;
-                letter2 = Letter2;
-                letter3 = Letter3;
-                letter4 = Letter4;
-                letter5 = Letter5;
-            }
 
         }
+
+        public class Guesses
+        {
+            List<string> guesses = new List<string>();
+
+            public static void Guess(string word)
+            {
+
+                for (int i = 0; i < word.Length; i++)
+                {
+                    if (i == 5)
+                    { 
+                        if (char.IsLetter(word[i]) == true)
+                        {
+                            Guesses guesses = new Guesses();
+                            guesses.guesses.Add(word);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Nem alkalmas szó!");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Nem alkalmas szó!");
+                    }
+
+                }
+
+                
+            }
+        }
+
+
         static void Main(string[] args)
         {
             string[] szavak = File.ReadAllLines("words.txt");
@@ -36,7 +53,7 @@
             }
             */
 
-            Console.WriteLine(szavak[1][2]); 
+            Console.WriteLine(szavak[1][2]);
             /*majd megcsinaljuk hogy random képes legyen kiválasztani egy szót és mentse is el hogy az hányadik szó
              * azután eltudjuk érni az összes ebtüjét annak a szónak egy for ciklussal,
              switch case-t fogok majd használni arra hogy megnézze van e olyan betu a szóban és hogy jó helyen van e,
